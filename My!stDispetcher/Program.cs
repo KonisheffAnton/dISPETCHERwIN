03f13d9b865fc3e0e253e07e34b88b138cb8fa92
KonisheffAnton <konysheffanton@yandex.ru>
2021-08-15 18:01:17 +0300

    15.08.21

--- My!stDispetcher/Program.cs
+++ My!stDispetcher/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace My_stDispetcher
 {
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            comand newCommand = new comand();
+            newCommand.parseParameters(args);
+            TimerCallback tm = new TimerCallback(deleteProc);
+            Timer timer = new Timer(tm, newCommand, 0, newCommand.Parameter3);
+            Console.ReadLine();
+        }
+
+        public static void deleteProc(object sender)
+        {
+            comand newCommand = new comand();
+            newCommand = (comand)sender;
+
+            Process[] listprosecc = Process.GetProcesses();
+            foreach (Process oneproc in listprosecc)
+            {
+                string ProsessName = oneproc.ProcessName;
+
+                ProsessName = ProsessName.ToLower();
+                if (ProsessName.Equals(newCommand.Parameter1))
+                {
+                    Console.WriteLine((DateTime.Now - oneproc.StartTime).TotalSeconds + " " + (DateTime.Now - oneproc.StartTime).TotalMinutes);
+                   if ((DateTime.Now - oneproc.StartTime).TotalMinutes > newCommand.Parameter2){ oneproc.Kill(); }
+                    
+                }
+            }
         }
     }
-}
+    }
+
+
+
+           // int num = 0;
+            // устанавливаем метод обратного вызова
+          //  TimerCallback tm = new TimerCallback(delete);
+            // создаем таймер
+           // Timer timer = new Timer(tm, null, 0, 2000);
+
+    
---
+++ My!stDispetcher/comand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_stDispetcher
+{
+    class comand
+    {
+        public string Parameter1 { get; set; } = "0";
+        public int Parameter2 { get; set; } = 0;
+        public int Parameter3 { get; set; } = 0;
+
+
+        public void parseParameters(string[] innerArgs)
+        {
+            try
+            {
+                Convert.ToInt32(innerArgs[1]);
+                Convert.ToInt32(innerArgs[2]);
+
+               foreach (string arg in innerArgs)
+                { this.Parameter1 = innerArgs[0];
+                  this.Parameter2 = Convert.ToInt32(innerArgs[1]);
+                  this.Parameter3 = Convert.ToInt32(innerArgs[2]);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Неверный формат аргументов");
+            }
+        }
+    }
+}
